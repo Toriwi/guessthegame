59c81f1717238fb8e3440f265563ea837b99cfb9
Toriwi <viktoriavelikovav@gmail.com>
2020-11-06 15:10:55 +0200

    Add Button

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('message').textContent = 'Correct Number!'
+
+document.querySelector('number') = 13
